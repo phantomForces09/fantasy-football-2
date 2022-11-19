a0b992b83e6552f7384776f56a69bc4c9cdd5e17
phantomForces09 <103375096+phantomForces09@users.noreply.github.com>
2022-11-18 21:02:24 -0800

    changed to v2.0 version

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,13 +1,12 @@
 let energyTeam = 0;
-
 //localStorage.setItem("energy", 0);
-function start(){
-    let test = 0;
-    energyTeam=test;
-    document.getElementById("teamEnergy").innerHTML = energyTeam;
+function reset(){
+    let restart = 0;
+    energyTeam=restart;
+    document.getElementById("teamEnergy").innerHTML = restart;
 }
 function train(){
-    energyTeam+=1;
+    energyTeam-=1;
     document.getElementById("teamEnergy").innerHTML = energyTeam;
     localStorage.setItem("energy", energyTeam);
 }
@@ -15,12 +14,25 @@ function logout(){
     window.location.replace("index.html");
 }
 function saveTeam(){
-    localStorage.setItem("quarterback", document.getElementById("quarterback").value);
-    localStorage.setItem("runningback", document.getElementById("runner").value);
-    localStorage.setItem("widereceiver", document.getElementById("wide").value);
-    localStorage.setItem("guard", document.getElementById("guard").value);
-    document.getElementById("q").innerHTML = "<b>Quarterback:</b>" + localStorage.getItem("quarterback");
-    document.getElementById("r").innerHTML = "<b>Running Back:</b>" + localStorage.getItem("runningback");
-    document.getElementById("w").innerHTML = "<b>Wide Receiver:</b>" + localStorage.getItem("widereceiver");
-    document.getElementById("g").innerHTML = "<b>Guard:</b>" + localStorage.getItem("guard");
+    if(energyTeam>20){
+        localStorage.setItem("quarterback", document.getElementById("quarterback").value);
+        localStorage.setItem("runningback", document.getElementById("runner").value);
+        localStorage.setItem("widereceiver", document.getElementById("wide").value);
+        localStorage.setItem("guard", document.getElementById("guard").value);
+        document.getElementById("list").innerHTML = "<b>Quarterback: </b>" + localStorage.getItem("quarterback") + "<br><b>Runningback: </b>" + localStorage.getItem("runningback") + "<br><b>Wide Receiver: </b>" + localStorage.getItem("widereceiver") + "<br><b>Guard: </b>" + localStorage.getItem("guard");
+        energyTeam-=20;
+        document.getElementById("teamEnergy").innerHTML = energyTeam;
+        localStorage.setItem("energy", energyTeam);
+    }else{
+        alert("you don't have enough credits, dumbass");
+    }
+}
+function createTeam(){
+    let teamName = prompt("rename team!");
+    localStorage.setItem("teamName", teamName);
+    document.getElementById("teamName").innerHTML = localStorage.getItem("teamName");
+    document.title = "FFII | " + localStorage.getItem("username") + "'s " + localStorage.getItem("teamName");
+}
+function newGame(){
+    document.getElementById("energy").innerHTML = "loser";
 }
